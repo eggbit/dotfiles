80ebacd8cdf0daa25a860ecd84e5ef2d2871f871
eggbit <gitrod@gmail.com>
2016-10-21 16:33:21 -0400

    fish: Proper handing of appending to PATH.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,13 @@
-set PATH $PATH /usr/local/sbin
-set PATH $PATH /Applications/MAMP/bin/php/php5.6.10/bin
-set PATH $PATH /Applications/MAMP/Library/bin/
+set -l paths /usr/local/sbin \
+                    /sbin \
+                    /Applications/MAMP/bin/php/php5.6.10/bin \
+                    /Applications/MAMP/Library/bin/
+
+for e in $paths
+    if test -d $e
+        set PATH $PATH $e
+    end
+end
 
 alias tearoot="cd ~/sites/clients/tea"
 alias optipng="optipng -o5 -strip all"
